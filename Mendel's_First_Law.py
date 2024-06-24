0660c229f78245c9f2cfe6ef136a23d198a13641
Sung Hoon Choi <50727298+sungh7@users.noreply.github.com>
2024-06-25 00:44:44 +0900

    Update Mendel's_First_Law.py

--- Mendel's_First_Law.py
+++ Mendel's_First_Law.py
@@ -1,15 +1,15 @@
 k, m, n = 15, 28, 24
 
-#case 1 k + alpha
+#case 1 k + *
 case1 = (k/(k+m+n)) * ((k-1)/(k-1+m+n)) \
 + (k/(k+m+n)) * (m/(k-1+m+n)) * 2 \
 + (k/(k+m+n)) * (n/(k-1+m+n)) * 2
 
-#case 2 domi + rece
+#case 2 m + (m,n)
 case2 = (m/(k+m+n)) * ((m-1)/(k+m+n-1)) * (3/4) \
 + (m/(k+m+n)) * (n/(k+m+n-1)) * 2 * (2/4)
 
-#case 3 hetero + rece
+#case 3 n + n
 case3 = (n/(k+m+n)) * ((n-1)/(k+m+n-1))
 
 case1 + case2
